43af11fd16b2d594372e1fa9b6d6203a89c9c111
HenkvanAsperen <31693834+HenkvanAsperen@users.noreply.github.com>
2019-03-14 11:39:00 +0100

    update app

--- app/src/main/java/nl/experiment/voorbeeldapp/Settingspage.java
+++ app/src/main/java/nl/experiment/voorbeeldapp/Settingspage.java
@@ -3,13 +3,28 @@ package nl.experiment.voorbeeldapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Settingspage extends AppCompatActivity {
+
+    ImageButton backtomap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settingspage);
+
+
+            backtomap = (ImageButton) findViewById(R.id.backtomap);
+            backtomap.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intentLoadNewActivity = new Intent(Settingspage.this, Mappage.class);
+                    startActivity(intentLoadNewActivity);
+                }
+            });
     }
 }
